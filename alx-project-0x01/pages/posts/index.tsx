1023ba5fd08e135b45f047976843248fe4d26c0e
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-05-31 16:00:04 +0300

    Posts and PostCard edited - Task 2

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,15 +1,43 @@
-import React from "react";
-import PostCard from "../../components/common/PostCard";
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const PostsPage: React.FC = () => {
+interface PostsProps {
+    posts: PostProps[];
+}
+
+const Posts: React.FC<PostsProps> = ({ posts }) => {
+  console.log(posts)
   return (
-    <div className="p-8">
-        <Header/>
-      <h1 className="text-2xl font-bold mb-4">Posts</h1>
-      <PostCard />
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
+      </main>
     </div>
-  );
-};
+  )
+}
 
-export default PostsPage;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Posts;
