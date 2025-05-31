9cf5130e5fc740b153144035b329bd12fcac80be
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-05-31 15:47:21 +0300

    Header and homepage - Task 1

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostCard from "../../components/common/PostCard";
+import Header from "@/components/layout/Header";
 
 const PostsPage: React.FC = () => {
   return (
     <div className="p-8">
+        <Header/>
       <h1 className="text-2xl font-bold mb-4">Posts</h1>
       <PostCard />
     </div>
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layout/Header";
 import React from "react";
 
 const UsersPage: React.FC = () => {
   return (
     <div className="p-8">
+        <Header/>
       <h1 className="text-2xl font-bold mb-4">Users</h1>
       <p className="text-gray-600">User list will go here.</p>
     </div>
